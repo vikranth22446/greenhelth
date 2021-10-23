18ca9f0fba4b6bfcd8601b6012fa5a6f85a7a64f
yousefh409 <yousefh409@gmail.com>
2021-10-22 22:41:12 -0700

    initial AR stuff set up

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,36 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:green_helth/Screens/badge_detection_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GreenHelth',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new AugmentedFacesScreen();
+  }
+}
